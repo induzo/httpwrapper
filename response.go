a22183934a40ba0026fd18943087b4caf565389f
Vincent Serpoul <vincent@serpoul.com>
2022-04-07 23:48:33 +0800

    feature: first commit

---
+++ response.go
@@ -0,0 +1,22 @@
+package httpwrapper
+
+import (
+	"net/http"
+
+	"github.com/rs/zerolog"
+)
+
+type Response struct {
+	Body           any
+	HTTPStatusCode int
+}
+
+func (hr *Response) render(log zerolog.Logger, w http.ResponseWriter, r *http.Request) {
+	render(
+		log,
+		r.Header.Get("Accept"),
+		hr.HTTPStatusCode,
+		hr.Body,
+		w,
+	)
+}
